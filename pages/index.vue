167d5ea15f62b111d4805156f6e1f9959a4ebd81
AZH0823 <qoo9986360@gmail.com>
2023-04-09 13:21:42 +0800

    init Nuxt2 and Tailwind@2.2 workspace and config Setting

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  <div class="divide-y divide-teal-400 md:divide-pink-400">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+  </div>
+
+    <h1 class="t">Hello Tailwind</h1>
+    <Tutorial/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
